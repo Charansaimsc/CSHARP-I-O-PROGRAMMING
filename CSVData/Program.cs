63aa09284fc890d100c7ab5e991e04b0577f72be
Charansaimsc <charanmsc7969@gmail.com>
2026-02-09 14:28:06 +0530

    [charan]Add. Main Method for all the Problems on CSV DATA

---
+++ CSVData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //     Student.ReadPrint();
+
+
+        //  StudentDataAdd. WriteData();
+
+
+        // ReadAndCountRows.WorkOn();
+
+        FilterRecords.Run();
+    }
+}
