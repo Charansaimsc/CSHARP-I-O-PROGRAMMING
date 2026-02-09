838d2183da8c27b1511d3f6d67085ba3223ff5c6
Charansaimsc <charanmsc7969@gmail.com>
2026-02-09 12:49:51 +0530

    [charan]Add. Main Method For all probelms on Streams

---
+++ streams/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static void Main()
+    {
+        // string sourceFile = "input.txt";
+        // string destinationFile = "output.txt";
+
+        // ReadAndWrite.CopyFile(sourceFile, destinationFile);
+
+        // string sourceFile = "input.txt";
+        // string destUnbuffered = "output.txt";
+        // string destBuffered = "output.txt";
+
+        // Stopwatch stopwatch = new Stopwatch();
+
+
+        // stopwatch.Start();
+        // FileCopier.CopyWithoutBuffer(sourceFile, destUnbuffered);
+        // stopwatch.Stop();
+        // Console.WriteLine("Unbuffered Time: " + stopwatch.ElapsedMilliseconds + " ms");
+
+        // stopwatch.Reset();
+
+
+        // stopwatch.Start();
+        // FileCopier.CopyWithBuffer(sourceFile, destBuffered);
+        // stopwatch.Stop();
+        // Console.WriteLine("Buffered Time  : " + stopwatch.ElapsedMilliseconds + " ms");
+
+
+        // User.ReadAndSave();
+
+
+        // EmployeeService.Run();
+
+
+        // ImageByteArrayService.Run();
+
+
+        // Text.Run();
+
+
+        // StudentDataService.Run();
+
+
+
+
+        WordFrequencyService.Run();
+    }
+}
